bdbf5704ffed77e4000d1c33fa91b24bede1c8d0
EmanAmr1 <emanamr543@gmail.com>
2024-10-20 10:11:52 +0300

    project init

---
+++ OrderFulfillmentSystem/src/main/java/com/parsing/OrderFulfillmentSystem/OrderFulfillmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parsing.OrderFulfillmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFulfillmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFulfillmentSystemApplication.class, args);
+	}
+
+}
---
+++ OrderFulfillmentSystem/src/test/java/com/parsing/OrderFulfillmentSystem/OrderFulfillmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsing.OrderFulfillmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFulfillmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
