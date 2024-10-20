a0e7d3f7860d8d6c1c7af81c17c4f536f5201a02
EmanAmr1 <emanamr543@gmail.com>
2024-10-20 11:06:58 +0300

    Load Csv Files

---
+++ OrderFulfillmentSystem/src/main/java/com/parsing/OrderFulfillmentSystem/Service/Impl/LoadCsvFilesServiceImpl.java
@@ -0,0 +1,38 @@
+package com.parsing.OrderFulfillmentSystem.Service.Impl;
+
+import com.parsing.OrderFulfillmentSystem.Service.LoadCsvFilesService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import static com.parsing.OrderFulfillmentSystem.Shared.sharedObjects.INSTANCE;
+
+@Service
+public class LoadCsvFilesServiceImpl implements LoadCsvFilesService {
+
+    @Override
+    public void loadCsvFiles(String Directory) {
+        try{
+            Files.walkFileTree(Paths.get(Directory) , new SimpleFileVisitor<Path>() {
+
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+                    String fileName = file.getFileName().toString();
+
+                    if(fileName.endsWith(".csv")){
+
+                        if(fileName.toLowerCase().contains("order")) INSTANCE.orderFiles.add(fileName);
+                        if(fileName.toLowerCase().contains("product")) INSTANCE.productFiles.add(fileName);
+                        if(fileName.toLowerCase().contains(("shipment"))) INSTANCE.shipmentFiles.add(fileName);
+                        if(fileName.toLowerCase().contains(("invoice"))) INSTANCE.invoiceFiles.add(fileName);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+                   
+            });
+        }catch (Exception e){
+            System.out.println("Error while loading files: " + e.getMessage());
+        }
+    }
+}
---
+++ OrderFulfillmentSystem/src/main/java/com/parsing/OrderFulfillmentSystem/Service/LoadCsvFilesService.java
@@ -0,0 +1,6 @@
+package com.parsing.OrderFulfillmentSystem.Service;
+
+public interface LoadCsvFilesService {
+
+    public void loadCsvFiles(String Directory);
+}
