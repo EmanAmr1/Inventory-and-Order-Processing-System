62d1ea14b551bbb933363e1b6109e3e72363fc03
EmanAmr1 <emanamr543@gmail.com>
2024-10-20 10:49:44 +0300

    add enum for shared Objects

---
+++ OrderFulfillmentSystem/src/main/java/com/parsing/OrderFulfillmentSystem/Shared/sharedObjects.java
@@ -0,0 +1,34 @@
+package com.parsing.OrderFulfillmentSystem.Shared;
+
+import com.parsing.OrderFulfillmentSystem.Model.InvoiceModel;
+import com.parsing.OrderFulfillmentSystem.Model.OrderModel;
+import com.parsing.OrderFulfillmentSystem.Model.ProductModel;
+import com.parsing.OrderFulfillmentSystem.Model.ShipmentModel;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public enum sharedObjects {
+
+    INSTANCE;
+
+    public final BlockingQueue<OrderModel> orderQueue =new ArrayBlockingQueue<>(1000);
+    public final BlockingQueue<ProductModel> productQueue =new ArrayBlockingQueue<>(1000);
+    public final BlockingQueue<ShipmentModel> shipmentQueue =new ArrayBlockingQueue<>(1000);
+    public final BlockingQueue<InvoiceModel> invoiceQueue =new ArrayBlockingQueue<>(1000);
+
+    public final Deque<String> orderFiles = new ArrayDeque<>();
+    public final Deque<String> productFiles = new ArrayDeque<>();
+    public final Deque<String> shipmentFiles = new ArrayDeque<>();
+    public final Deque<String> invoiceFiles = new ArrayDeque<>();
+
+
+    public boolean isOrderFiles=false;
+    public boolean isProductFiles=false;
+    public boolean isShipmentFiles=false;
+    public boolean isInvoiceFiles=false;
+
+
+}
